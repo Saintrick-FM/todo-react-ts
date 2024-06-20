9176a2095210b9dabf8f50ce132e1b2de386e3b5
Francy Saintrick Malonga <55754974+Saintrick-FM@users.noreply.github.com>
2024-06-20 15:41:35 +0200

    Feature/welcome msg (#1)
    * ci(repository): add commitizen to the project
    
    Closes #1
    
    * feat(app.tsx): Set left space to the add button
    
    Closes #2
    
    * bump: version 0.1.0 → 0.2.0
    
    * feat(app.tsx): add welcome msg and delete app.test.tsx file
    
    * feat(app.tsx): delete unused imports
    
    * bump: version 0.2.0 → 0.3.0
    
    ---------
    
    Co-authored-by: Francy-malo <francy.malonga@nanocreatives.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import { FilterType, TaskType } from "./types";
@@ -39,8 +39,6 @@ function App() {
 
   useLayoutEffect(() => {
     getTodosFromApi();
-
-    // create_local_storage();
   }, []);
 
   // Sauvegarde des tâches dans le stockage local
@@ -95,6 +93,9 @@ function App() {
 
   return (
     <section className="container pt-5" id="todolist">
+      <h2 style={{ marginBottom: "20px", textAlign: "center" }}>
+        Welcome to our Todo app
+      </h2>
       <form className="d-flex pb-4">
         <input
           required={true}
@@ -109,6 +110,7 @@ function App() {
           id="add_task"
           className="btn btn-primary"
           onClick={(event: React.FormEvent) => submitTodo(event)}
+          style={{ marginLeft: 10 }}
         >
           Ajouter
         </button>
@@ -154,17 +156,6 @@ function App() {
               />
             );
           })}
-
-          {/* <!--   <li className="todo list-group-item d-flex align-items-center">
-              <input className="form-check-input" type="checkbox" id="todo-1"/>
-              <label className="ms-2 form-check-label" for="todo-1">
-                  Tâche à faire 2
-              </label>
-              <label className="ms-auto btn btn-danger btn-sm">
-              <i className="bi-trash">
-              </i>
-              </label>
-          </li> --> */}
         </ul>
       </main>
     </section>
